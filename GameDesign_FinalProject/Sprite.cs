4ab16cd4e340cddc45162ef74a9badb0852fb56b
Angie <angelitoregero@gmail.com>
2025-07-11 08:55:18 +0800

    added sprite class

---
+++ GameDesign_FinalProject/Sprite.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+namespace GameDesign_FinalProject
+{
+    internal class Sprite
+    {
+        private Vector2 position;
+        private Texture2D spriteImage;
+        private float spriteWidth;
+        private float spriteHeight;
+
+        public Sprite(Vector2 position)
+        {
+            this.position = position;
+        } 
+        public Vector2 Position { get => position; set => position = value; }
+        public Texture2D SpriteImage { get => spriteImage; set => spriteImage = value; }
+        public float SpriteWidth { get => spriteWidth; set => spriteWidth = value; }
+        public float SpriteHeight { get => spriteHeight; set => spriteHeight = value; }
+
+        public void Draw(GameTime gameTime, SpriteBatch spriteBatch)
+        {
+            spriteBatch.Draw(spriteImage, PositionRectangle, Color.White);
+        }
+
+        public Rectangle PositionRectangle
+        {
+            get { return new Rectangle((int)position.X, (int)position.Y, (int)spriteWidth, (int)spriteHeight); }
+        }
+
+    }
+}
